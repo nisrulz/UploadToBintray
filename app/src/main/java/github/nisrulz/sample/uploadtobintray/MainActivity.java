9eb1de309a43e33dd80a26153f1a7ae7c7777f99
Nishant Srivastava <nisrulz@gmail.com>
2016-07-11 14:11:33 +0530

    [*] implemented library method in app

--- app/src/main/java/github/nisrulz/sample/uploadtobintray/MainActivity.java
+++ app/src/main/java/github/nisrulz/sample/uploadtobintray/MainActivity.java
@@ -2,11 +2,14 @@ package github.nisrulz.sample.uploadtobintray;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import github.nisrulz.awesomelib.AwesomeLib;
 
 public class MainActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    AwesomeLib.getInstance().makeMeAwesome(this,"Nishant");
   }
 }
