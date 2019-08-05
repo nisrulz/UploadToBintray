d2554c0a4e7da817cc0a305abee3a9445b2fb7a2
Nishant Srivastava <nisrulz@gmail.com>
2019-08-05 23:02:49 +0200

    Updated awesome lib

--- awesomelib/src/main/java/github/nisrulz/awesomelib/AwesomeLib.java
+++ awesomelib/src/main/java/github/nisrulz/awesomelib/AwesomeLib.java
@@ -4,16 +4,20 @@ import android.content.Context;
 import android.widget.Toast;
 
 public class AwesomeLib {
-  private static AwesomeLib ourInstance = new AwesomeLib();
+    private static AwesomeLib ourInstance = new AwesomeLib();
 
-  public static AwesomeLib getInstance() {
-    return ourInstance;
-  }
+    public static AwesomeLib getInstance() {
+        return ourInstance;
+    }
 
-  private AwesomeLib() {
-  }
+    private AwesomeLib() {
+    }
 
-  public void makeMeAwesome(Context context, String data) {
-    Toast.makeText(context, "Awesome " + data, Toast.LENGTH_LONG).show();
-  }
+    public void makeMeAwesome(Context context, String data) {
+        Toast.makeText(context, "Awesome " + data, Toast.LENGTH_LONG).show();
+    }
+
+    public String getVersion() {
+        return BuildConfig.VERSION_NAME + "(" + BuildConfig.VERSION_CODE + ")";
+    }
 }
